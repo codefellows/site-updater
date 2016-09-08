ee481e99c5892893dd23579685fb105ad528af6a
Ivan Storck <ivan@ivanstorck.com>
2016-09-08 15:39:04 -0700

    test fire on every webhook

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ var server = require('contentful-webhook-server')()
 server.listen(3000, function(){
  console.log('Contentful webhook server running on port ' + 3000)
 })
+
+// Handler for all successful requests
+// Is not emitted when an error occurs
+server.on('ContentManagement.*', function(topic, req){
+  // topic is available as string
+  // => e.g. ContentManagement.Asset.unpublish
+  console.log('Request came in for: ' + topic);
+})
