91bb19974a4b7fc5b014da33ac260fd4b2a674e9
Ivan Storck <ivan@ivanstorck.com>
2016-09-08 17:06:45 -0700

    WIP on site update script

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var sh = require('shelljs')
+
 // Create server with default options
 var server = require('contentful-webhook-server')()
 
@@ -12,4 +14,12 @@ server.on('ContentManagement.*', function(topic, req){
   // topic is available as string
   // => e.g. ContentManagement.Asset.unpublish
   console.log('Request came in for: ' + topic);
+  // clone the repo
+  cloneResult = sh.exec('git clone git@bitbucket.org:code-fellows/website.git')
+  // make an empty commit
+  var commitResult = sh.exec('git commit --allow-empty -m \'Site update from contentful\'')
+  if (commitResult !== 0) {
+    // push the empty commit
+    var pushResult = sh.exec('git push origin master')
+  }
 })
