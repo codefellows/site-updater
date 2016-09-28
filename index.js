cb2ec786b3fde10e9a5e272f45e9915cefe4f558
Ivan Storck <ivan@ivanstorck.com>
2016-09-28 09:10:55 -0700

    Exit the process after successful update pm2 should restart it

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const safeJsonStringify = require('safe-json-stringify')
 // Requires a test to see if .env file exists because it will not on CI build
 if (fs.existsSync('.env')) require('dotenv').config()
 
+function timeout() {
+  console.error('Could not close connections in time, forcefully shutting down');
+  process.exit()
+}
+
 // Create server with default options
 const server = require('contentful-webhook-server')({
   username: process.env.BASIC_AUTH_USER,
@@ -22,9 +27,17 @@ server.listen(port, function listening() {
 server.on('ContentManagement.Entry.unpublish', function unpublishReq(req) {
   console.log(`An entry was unpublished: ${safeJsonStringify(req)}`)
   updater()
+  server.close(function processExit() {
+    process.exit()
+  })
+  setTimeout(timeout, 10 * 1000)
 })
 
 server.on('ContentManagement.Entry.publish', function publishReq(req) {
   console.log(`An entry was published: ${safeJsonStringify(req)}`)
   updater()
+  server.close(function processExit() {
+    process.exit()
+  })
+  setTimeout(timeout, 10 * 1000)
 })
